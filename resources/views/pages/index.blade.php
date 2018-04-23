42dd226b2a9056eaa29ba5028e04c1b7d8d07168
Pitch04 <trickypat04@gmail.com>
2018-04-24 01:53:55 +0800

    First commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+ <!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name', 'VIREM')}}</title>
+    </head>
+    <body>
+       <h1>Welcome to Virem</h1>
+       <p>This is the Virem app from Laravel from Scratch</p>
+    </body>
+</html>
